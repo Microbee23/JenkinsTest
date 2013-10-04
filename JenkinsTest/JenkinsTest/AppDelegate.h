68de6738f6a2585673fe195a501da35ab297f389
Microbee23 <info@arcadewizzkid.com>
2013-10-04 21:11:56 +1000

    Xcode Project created

---
+++ JenkinsTest/JenkinsTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JenkinsTest
+//
+//  Created by frog on 4/10/13.
+//  Copyright (c) 2013 Arcade Wizz Kid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ JenkinsTest/JenkinsTest/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  JenkinsTest
+//
+//  Created by frog on 4/10/13.
+//  Copyright (c) 2013 Arcade Wizz Kid. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
